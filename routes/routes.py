b3d7bdd9a2d4c936df524222c9a19e262d6656b4
Sandesh <pathak2057@gmail.com>
2023-06-16 11:38:21 +0545

    Login Corrected

--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask 
 import flask 
 import json
-
+import re
 
 app= Flask(
     __name__,
@@ -33,6 +33,25 @@ def post_signup():
     print("---")
 
 
+    regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+    if re.fullmatch(regex, email_sign):
+        print("Valid email")
+    else:
+        return flask.render_template("errorpage.html")
+    if len(name or password) < 9:
+        return flask.render_template("errorpage.html")
+    
+
+    with open('data.json') as user_file:
+        file_contents = user_file.read()
+
+    parsed_json = json.loads(file_contents)
+ 
+    for i in parsed_json['user_records']:
+        if (i['email_sign']==email_sign):
+            return flask.render_template("errorpage.html")
+
+
     def write_json(new_data, filename='data.json'):
         with open(filename,'r+') as file:
             # First we load existing data into a dict.
@@ -44,15 +63,10 @@ def post_signup():
             # convert back to json.
             json.dump(file_data, file, indent = 4)
     
-    
+
     write_json(signup_data)
 
-
-    # if len(name or password) < 9:
-    #     return flask.render_template("errorpage.html")
-    # else:
     return flask.render_template("homepage.html")
-    
 
 @app.route("/")
 @app.route("/homepage")
@@ -64,23 +78,39 @@ def home():
 @app.route("/login")
 def login():
     print('Get----')
-    with open('data.json') as user_file:
-        file_contents = user_file.read()
-
-    parsed_json = json.loads(file_contents)
-  
-    for i in parsed_json['user_records']:
-        print(i['name'])
-
     return flask.render_template("login.html",action="/login_post")
 
 
 @app.route("/login_post", methods = ["POST"])
 def post_login():
     print('post----')
-    name = flask.request.form["username"]
+    email_sign= flask.request.form["email_signup"]
     password = flask.request.form["password"]
-    print("name is =",name)
+    print("name is =",email_sign)
     print("password is =",password)
-    return flask.render_template("Welcomepage.html")
+
+    with open('data.json') as user_file:
+        file_contents = user_file.read()
+
+    parsed_json = json.loads(file_contents)
+    print(parsed_json)
     
+    for i in parsed_json['user_records']:
+
+        print("here",i["name"], i["password"])
+        
+        if(i['email_sign']==email_sign and i['password']==password):
+
+            print("signed_email is",i['email_sign'])
+            print("signed_password is",i['password'])
+            login_status = "True"
+        else:
+            print("error signed_email is",i['email_sign'])
+            print("eeror signed_password is",i['password'])
+            login_status = "False"
+    if login_status =="True":  
+        return flask.render_template("Welcomepage.html") 
+    else:
+        return flask.render_template("errorpage.html")  
+
+
